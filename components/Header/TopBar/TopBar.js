672a24c07c252504ec492c19d8ee9bf43e92a79a
Alfredo Fernandez <alfredo@hotmail.com>
2021-04-21 12:08:19 -0400

    header structure

---
+++ components/Header/TopBar/TopBar.js
@@ -0,0 +1,7 @@
+export default function TopBar() {
+  return (
+    <div>
+      <h1>TopBar...</h1>
+    </div>
+  )
+}
--- layouts/BassicLayout/BasicLayout.js
+++ layouts/BassicLayout/BasicLayout.js
@@ -1,11 +1,12 @@
-import React from "react"
 import { Container } from "semantic-ui-react"
+import Header from "../../components/Header"
 
 export default function BasicLayout(props) {
   const { children } = props;
 
   return (
-    <Container fuild="true" className="basic-layout">
+    <Container fluid className="basic-layout">
+      <Header />
       <Container className="content">{children}</Container>
     </Container>
   )
