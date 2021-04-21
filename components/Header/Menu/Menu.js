766b7b9d7a7ef5458124953f642506c3d705c1ae
Alfredo Fernandez <alfredo@hotmail.com>
2021-04-21 14:11:39 -0400

    add menu web

---
+++ components/Header/Menu/Menu.js
@@ -0,0 +1,46 @@
+import { Container, Menu, Grid, Icon, Label } from "semantic-ui-react";
+import Link from "next/link";
+
+export default function MenuWeb() {
+  return (
+    <div className="menu">
+      <Container>
+        <Grid>
+          <Grid.Column className="menu__left" width={6}>
+            <MenuPlatforms />
+          </Grid.Column>
+          <Grid.Column className="menu__right" width={10}>
+            <MenuOptions />
+          </Grid.Column>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+function MenuPlatforms() {
+  return (
+    <Menu>
+      <Link href="/play-station">
+        <Menu.Item as="a">PlayStation</Menu.Item>
+      </Link>
+      <Link href="/xbox">
+        <Menu.Item as="a">Xbox</Menu.Item>
+      </Link>
+      <Link href="/switch">
+        <Menu.Item as="a">Switch</Menu.Item>
+      </Link>
+    </Menu>
+  )
+}
+
+function MenuOptions() {
+  return (
+    <Menu>
+      <Menu.Item>
+        <Icon name="user outline" />
+        Mi cuenta
+      </Menu.Item>
+    </Menu>
+  )
+}
