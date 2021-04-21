5d829f19a308c708017aafec5a04059124d99b0f
Alfredo Fernandez <alfredo@hotmail.com>
2021-04-21 12:45:53 -0400

    add topBar Logo style

--- components/Header/TopBar/TopBar.js
+++ components/Header/TopBar/TopBar.js
@@ -1,7 +1,29 @@
+import { Container, Grid, Image, Input } from "semantic-ui-react";
+import Link from "next/link";
+
 export default function TopBar() {
   return (
-    <div>
-      <h1>TopBar...</h1>
+    <div className="top-bar">
+      <Container>
+        <Grid className="top-bar">
+          <Grid.Column width={8} className="top-bar__left">
+            <Logo />
+          </Grid.Column>
+          <Grid.Column width={8} className="top-bar__right">
+            <h2>Buscador</h2>
+          </Grid.Column>
+        </Grid>
+      </Container>
     </div>
   )
+}
+
+function Logo() {
+  return (
+    <Link href="/">
+      <a>
+        <Image src="/logo.png" alt="Gaming" />
+      </a>
+    </Link>
+  );
 }
