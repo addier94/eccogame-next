cab7a557636978ecc23699fd230223ef062ef5e1
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-22 12:18:43 -0400

    Auth Fetch valid user auth

--- api/token.js
+++ api/token.js
@@ -1,4 +1,5 @@
 import { TOKEN } from "../utils/constants";
+import jwtDecode from "jwt-decode";
 
 export function setToken(token) {
   localStorage.setItem(TOKEN, token);
@@ -6,8 +7,18 @@ export function setToken(token) {
 
 export function getToken() {
   return localStorage.getItem(TOKEN);
-} 
+}
 
 export function removeToken() {
   localStorage.removeItem(TOKEN);
+}
+
+export function hasExpiredToken(token) {
+  const tokenDecode = jwtDecode(token);
+  const expireDate = tokenDecode.exp * 1000;
+  const currentDate = new Date().getTime();
+  if (currentDate > expireDate) {
+    return true;
+  }
+  return false;
 }
