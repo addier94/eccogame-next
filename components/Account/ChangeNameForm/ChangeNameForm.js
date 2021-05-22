e5dd167275619022fd2999eea1f880f001f7b125
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-22 19:57:20 -0400

    handle form for user

--- components/Account/ChangeNameForm/ChangeNameForm.js
+++ components/Account/ChangeNameForm/ChangeNameForm.js
@@ -1,26 +1,56 @@
 import React from "react";
 import { Form, Button } from "semantic-ui-react";
+import { Formik, useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
 
 export default function ChangeNameForm(props) {
   const { user } = props;
+
+  const formik = useFormik({
+    initialValues: initialValues(user.name, user.lastname),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
+
   return (
     <div className="change-name-form">
       <h4>Cambia tu nombre y apellidos</h4>
-      <Form>
+      <Form onSubmit={formik.handleSubmit}>
         <Form.Group widths="equal">
           <Form.Input
             name="name"
             placeholder="Tu nuevo nombre"
-            value={user.name}
+            onChange={formik.handleChange}
+            value={formik.values.name}
+            error={formik.errors.name}
           />
           <Form.Input
             name="lastname"
             placeholder="Tus nuevos apellidos"
-            value={user.lastname}
+            onChange={formik.handleChange}
+            value={formik.values.lastname}
+            error={formik.errors.lastname}
           />
         </Form.Group>
         <Button className="submit">Actualizar</Button>
       </Form>
     </div>
   );
+}
+
+function initialValues(name, lastname) {
+  return {
+    name: name || "",
+    lastname: lastname || "",
+  };
+}
+
+function validationSchema() {
+  return {
+    name: Yup.string().required(true),
+    lastname: Yup.string().required(true),
+  };
 }
