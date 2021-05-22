4fcb1a4b03481471c6b99a0215a206156196aea2
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-22 16:02:19 -0400

    add classNames package

--- layouts/BassicLayout/BasicLayout.js
+++ layouts/BassicLayout/BasicLayout.js
@@ -1,13 +1,22 @@
-import { Container } from "semantic-ui-react"
-import Header from "../../components/Header"
+import { Container } from "semantic-ui-react";
+import classNames from "classnames";
+import Header from "../../components/Header";
 
 export default function BasicLayout(props) {
-  const { children } = props;
+  const { children, className } = props;
 
   return (
-    <Container fluid className="basic-layout">
+    <Container
+      fluid
+      className={
+        (classNames("basic-layout"),
+        {
+          [className]: className,
+        })
+      }
+    >
       <Header />
       <Container className="content">{children}</Container>
     </Container>
-  )
+  );
 }
---
+++ pages/account.js
@@ -0,0 +1,10 @@
+import React from "react";
+import BasicLayout from "../layouts/BassicLayout";
+
+export default function Account() {
+  return (
+    <BasicLayout className="account">
+      <h1>Estamos en mi cuenta</h1>
+    </BasicLayout>
+  );
+}
