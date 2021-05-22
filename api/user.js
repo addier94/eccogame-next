19fe74a787ffe8749792a0c8f94a6a823f57d9b1
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-22 15:24:22 -0400

    options for menu user in login

--- api/user.js
+++ api/user.js
@@ -1,4 +1,5 @@
 import { BASE_PATH } from "../utils/constants";
+import { authFetch } from "../utils/fetch";
 
 export async function registerApi(formData) {
   try {
@@ -38,15 +39,15 @@ export async function loginApi(formData) {
   }
 }
 
-export async function resetPasswordApi (email) {
-  try{
+export async function resetPasswordApi(email) {
+  try {
     const url = `${BASE_PATH}/auth/forgot-password`;
     const params = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({email}),
+      body: JSON.stringify({ email }),
     };
     // console.log(url, params);
     const response = await fetch(url, params);
@@ -54,6 +55,16 @@ export async function resetPasswordApi (email) {
     return result;
   } catch (error) {
     console.log(error);
+    return null;
+  }
+}
+
+export async function getMeApi(logout) {
+  try {
+    const url = `${BASE_PATH}/users/me`;
+    const result = await authFetch(url, null, logout);
+    return result ? result : null;
+  } catch (error) {
     return null;
   }
 }
--- components/Header/Menu/Menu.js
+++ components/Header/Menu/Menu.js
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Menu, Grid, Icon, Label } from "semantic-ui-react";
 import Link from "next/link";
 import BasicModal from "../../Modal/BasicModal";
 import Auth from "../../Auth";
 import useAuth from "../../../hooks/useAuth";
+import { getMeApi } from "../../../api/user";
 
 export default function MenuWeb() {
   const [showModal, setShowModal] = useState(false);
   const [titleModal, setTitleModal] = useState("Iniciar sesión");
+  const [user, setUser] = useState(undefined);
   const { auth, logout } = useAuth();
+
+  useEffect(() => {
+    (async () => {
+      const response = await getMeApi(logout);
+      setUser(response);
+    })();
+  }, [auth]); // se ejecuta siempre y cuando el AUTH cambie o el token se ha actualizado
 
   const onShowModal = () => setShowModal(true);
   const onCloseModal = () => setShowModal(false);
@@ -21,10 +30,12 @@ export default function MenuWeb() {
             <MenuPlatforms />
           </Grid.Column>
           <Grid.Column className="menu__right" width={10}>
-            {auth ? (
-              <button onClick={logout}>Cerrar sesión</button>
-            ) : (
-              <MenuOptions onShowModal={onShowModal} />
+            {user !== undefined && (
+              <MenuOptions
+                onShowModal={onShowModal}
+                user={user}
+                logout={logout}
+              />
             )}
           </Grid.Column>
         </Grid>
@@ -58,14 +69,45 @@ function MenuPlatforms() {
 }
 
 function MenuOptions(props) {
-  const { onShowModal } = props;
+  const { onShowModal, user, logout } = props;
 
   return (
     <Menu>
-      <Menu.Item onClick={onShowModal}>
-        <Icon name="user outline" />
-        Mi cuenta
-      </Menu.Item>
+      {user ? (
+        <>
+          <Link href="/orders">
+            <Menu.Item as="a">
+              <Icon name="game" />
+              Mis pedidos
+            </Menu.Item>
+          </Link>
+          <Link href="/wishlist">
+            <Menu.Item as="a">
+              <Icon name="heart outline" />
+              Wishlist
+            </Menu.Item>
+          </Link>
+          <Link href="/account">
+            <Menu.Item as="a">
+              <Icon name="user outline" />
+              {user.name} {user.lastname}
+            </Menu.Item>
+          </Link>
+          <Link href="/cart">
+            <Menu.Item as="a" className="m-0">
+              <Icon name="cart" />
+            </Menu.Item>
+          </Link>
+          <Menu.Item onClick={logout} className="m-0">
+            <Icon name="power off" />
+          </Menu.Item>
+        </>
+      ) : (
+        <Menu.Item onClick={onShowModal}>
+          <Icon name="user outline" />
+          Mi cuenta
+        </Menu.Item>
+      )}
     </Menu>
   );
 }
