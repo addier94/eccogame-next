a034945de524ca164a9b2437c5681160bd316ea9
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-23 07:21:11 -0400

    handle form for email

--- components/Account/ChangeEmailForm/ChangeEmailForm.js
+++ components/Account/ChangeEmailForm/ChangeEmailForm.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
 
 export default function ChangeEmailForm(props) {
   const { user, logout, setReloadUser } = props;
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
 
   return (
     <div className="change-email-form">
@@ -10,16 +21,45 @@ export default function ChangeEmailForm(props) {
         Cambia tu e-mail <span>(Tu e-mail actual: {user.email})</span>
       </h4>
 
-      <Form>
+      <Form onSubmit={formik.handleSubmit}>
         <Form.Group widths="equal">
-          <Form.Input name="email" placeholder="Tu numero e-email" />
+          <Form.Input
+            name="email"
+            placeholder="Tu nuevo e-email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+            error={formik.errors.email}
+          />
           <Form.Input
             name="repeatEmail"
             placeholder="Confirma tu nuevo e-mail"
+            onChange={formik.handleChange}
+            value={formik.values.repeatEmail}
+            error={formik.errors.repeatEmail}
           />
         </Form.Group>
         <Button className="submit">Actualizar</Button>
       </Form>
     </div>
   );
+}
+
+function initialValues() {
+  return {
+    email: "",
+    repeatEmail: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    email: Yup.string()
+      .email(true)
+      .required(true)
+      .oneOf([Yup.ref("repeatEmail")], "Email de be ser el mismo"),
+    repeatEmail: Yup.string()
+      .email(true)
+      .required(true)
+      .oneOf([Yup.ref("email")], true),
+  };
 }
