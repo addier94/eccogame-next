093f3b3af2ec7ad729e8cc54dda3520943f72199
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-24 08:12:22 -0400

    list addresses

--- components/Account/ListAddress/ListAddress.js
+++ components/Account/ListAddress/ListAddress.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Grid, Button } from "semantic-ui-react";
+import { map, size } from "lodash";
 import { getAddressesApi } from "../../../api/address";
 import useAuth from "../../../hooks/useAuth";
 
@@ -14,8 +16,38 @@ export default function ListAddress() {
   }, []);
 
   return (
-    <div>
-      <h1>List Address.....</h1>
+    <div className="list-address">
+      {size(addresses) === 0 ? (
+        <h3>No hay ninguna dirección creada</h3>
+      ) : (
+        <Grid>
+          {map(addresses, (address) => (
+            <Grid.Column key={address.id} mobile={16} tablet={8} computer={4}>
+              <Address address={address} />
+            </Grid.Column>
+          ))}
+        </Grid>
+      )}
+    </div>
+  );
+}
+
+function Address(props) {
+  const { address } = props;
+
+  return (
+    <div className="address">
+      <p>{address.title}</p>
+      <p>{address.name}</p>
+      <p>{address.address}</p>
+      <p>
+        {address.state}, {address.city} {address.postalCode}
+      </p>
+      <p>{address.phone}</p>
+      <div className="actions">
+        <Button primary>Editar</Button>
+        <Button>Eliminar</Button>
+      </div>
     </div>
   );
 }
