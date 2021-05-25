e78a5deedbb6f4be835fa7b87aaab48c4c4f4e54
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-24 22:52:41 -0400

    delete address

--- api/address.js
+++ api/address.js
@@ -30,3 +30,21 @@ export async function getAddressesApi(idUser, logout) {
     return null;
   }
 }
+
+export async function deleteAddressApi(idAddress, logout) {
+  try {
+    const url = `${BASE_PATH}/addresses/${idAddress}`;
+    const params = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const result = await authFetch(url, params, logout);
+    if (result.statusCode === 500) throw "Error del servidor";
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
--- components/Account/ListAddress/ListAddress.js
+++ components/Account/ListAddress/ListAddress.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Grid, Button } from "semantic-ui-react";
 import { map, size } from "lodash";
-import { getAddressesApi } from "../../../api/address";
+import { getAddressesApi, deleteAddressApi } from "../../../api/address";
 import useAuth from "../../../hooks/useAuth";
 
 export default function ListAddress(props) {
@@ -27,7 +27,11 @@ export default function ListAddress(props) {
         <Grid>
           {map(addresses, (address) => (
             <Grid.Column key={address.id} mobile={16} tablet={8} computer={4}>
-              <Address address={address} />
+              <Address
+                address={address}
+                logout={logout}
+                setReloadAddresses={setReloadAddresses}
+              />
             </Grid.Column>
           ))}
         </Grid>
@@ -37,7 +41,15 @@ export default function ListAddress(props) {
 }
 
 function Address(props) {
-  const { address } = props;
+  const { address, logout, setReloadAddresses } = props;
+  const [loadingDelete, setLoadingDelete] = useState(false);
+
+  const deleteAddress = async () => {
+    setLoadingDelete(true);
+    const response = await deleteAddressApi(address._id, logout);
+    if (response) setReloadAddresses(true);
+    setLoadingDelete(false);
+  };
 
   return (
     <div className="address">
@@ -50,7 +62,9 @@ function Address(props) {
       <p>{address.phone}</p>
       <div className="actions">
         <Button primary>Editar</Button>
-        <Button>Eliminar</Button>
+        <Button onClick={deleteAddress} loading={loadingDelete}>
+          Eliminar
+        </Button>
       </div>
     </div>
   );
