44358b72ffae370ad3473d1976ac39e90311e45e
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-26 09:22:27 -0400

    list structure and styles

--- components/ListGames/ListGames.js
+++ components/ListGames/ListGames.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Image, Grid } from "semantic-ui-react";
+import Link from "next/link";
 import { map } from "lodash";
 
 export default function ListGames(props) {
@@ -6,9 +8,38 @@ export default function ListGames(props) {
 
   return (
     <div className="list-games">
-      {map(games, (game) => (
-        <h3>{game.title}</h3>
-      ))}
+      <Grid>
+        <Grid.Row columns={5}>
+          {map(games, (game) => (
+            <Game game={game} />
+          ))}
+        </Grid.Row>
+      </Grid>
     </div>
+  );
+}
+
+function Game(props) {
+  const { game } = props;
+
+  return (
+    <Grid.Column className="list-games__game">
+      <Link href={`/${game.url}`}>
+        <a>
+          <div className="list-games__game-poster">
+            <Image src={game.poster.url} alt={game.title} />
+            <div className="list-games__game-poster-info">
+              {game.discount ? (
+                <span className="discount">-{game.discount}%</span>
+              ) : (
+                <span />
+              )}
+              <span className="price">{game.price}$us</span>
+            </div>
+          </div>
+          <h2>{game.title}</h2>
+        </a>
+      </Link>
+    </Grid.Column>
   );
 }
