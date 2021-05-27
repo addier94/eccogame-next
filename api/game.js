d6ef3e461f83aa501f8415f70d16e7073f40f339
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-26 22:06:10 -0400

    get game by platform

--- api/game.js
+++ api/game.js
@@ -13,3 +13,18 @@ export async function getLastGamesApi(limit) {
     return null;
   }
 }
+
+export async function getGamesPlatformApi(platform, limit, start) {
+  try {
+    const limitItems = `_limit=${limit}`;
+    const sortItems = `_sort=createdAt:desc`;
+    const startItems = `_start=${start}`;
+    const url = `${BASE_PATH}/games?platform.url=${platform}&${limitItems}&${sortItems}&${startItems}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
--- pages/games/[platformds].js
+++ pages/games/[platformds].js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import BasicLayout from "../../layouts/BassicLayout";
+import { getGamesPlatformApi } from "../../api/game";
+
+const limitPerPage = 10;
 
 export default function Platform() {
   const { query } = useRouter();
+  const [games, setGames] = useState(null);
 
+  useEffect(() => {
+    (async () => {
+      const response = await getGamesPlatformApi(
+        query.platformds,
+        limitPerPage,
+        0
+      );
+      setGames(response);
+    })();
+  }, [query]);
   return (
     <BasicLayout className="platform">
       <h1>Estamos en Plataformas: {query.platform} </h1>
