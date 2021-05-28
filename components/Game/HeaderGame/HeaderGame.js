d9b3b44cfe3292f0392d4a44bee56eb12f9ada6a
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-28 10:46:59 -0400

    add data to single product

--- components/Game/HeaderGame/HeaderGame.js
+++ components/Game/HeaderGame/HeaderGame.js
@@ -12,8 +12,37 @@ export default function HeaderGame(props) {
         <Image src={poster.url} alt={title} fluid />
       </Grid.Column>
       <Grid.Column mobile={16} tablet={10} computer={11}>
-        <p>Info Game</p>
+        <Info game={game} />
       </Grid.Column>
     </Grid>
+  );
+}
+
+function Info(props) {
+  const { game } = props;
+  const { title, summary, price, discount } = game;
+
+  return (
+    <>
+      <div className="header-game__title">
+        {title}
+        <Icon name="heart outline " link />
+      </div>
+      <div className="header-game__delivery">Entrega en 24/48h</div>
+      <div
+        className="header-game__summary"
+        dangerouslySetInnerHTML={{ __html: summary }}
+      />
+      <div className="header-game__buy">
+        <div className="header-game__buy-price">
+          <p>Precio de venta al publico: {price}$</p>
+          <div className="header-game__buy-price-actions">
+            <p>-{discount}%</p>
+            <p>{price - Math.floor(price * discount) / 100}$</p>
+          </div>
+        </div>
+        <Button className="header-game__buy-btn">Comprar</Button>
+      </div>
+    </>
   );
 }
