e3da4ed1a02e2734ff7bac546b607e5cb30ed2a4
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-28 06:47:08 -0400

    get single product

--- api/game.js
+++ api/game.js
@@ -40,3 +40,15 @@ export async function getTotalGamesPlatformApi(platform) {
     return null;
   }
 }
+
+export async function getGameByUrlApi(path) {
+  try {
+    const url = `${BASE_PATH}/games?url=${path}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result[0];
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
--- pages/[game].js
+++ pages/[game].js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import BasicLayout from "../layouts/BassicLayout";
+import { getGameByUrlApi } from "../api/game";
 
 export default function Game() {
+  const [game, setGame] = useState(null);
   const { query } = useRouter();
+
+  useEffect(() => {
+    (async () => {
+      const response = await getGameByUrlApi(query.game);
+      setGame(response);
+    })();
+  }, [query]);
 
   return (
     <BasicLayout className="game">
