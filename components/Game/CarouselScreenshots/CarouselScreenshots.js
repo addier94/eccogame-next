3a7f84f420ab0d78ed2e729d6d2e93e2f8edde03
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-29 06:50:59 -0400

    show image in modal

--- components/Game/CarouselScreenshots/CarouselScreenshots.js
+++ components/Game/CarouselScreenshots/CarouselScreenshots.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Image } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Image, Modal } from "semantic-ui-react";
 import Slider from "react-slick";
 import { map } from "lodash";
 
@@ -14,17 +14,29 @@ const settings = {
 
 export default function CarouselScreenshots(props) {
   const { title, screenshots } = props;
+  const [showModal, setShowModal] = useState(false);
+  const [urlImage, setUrlImage] = useState(null);
+
+  const openImage = (url) => {
+    setUrlImage(url);
+    setShowModal(true);
+  };
 
   return (
-    <Slider {...settings}>
-      {map(screenshots, (screenshot) => (
-        <Image
-          key={screenshot.id}
-          src={screenshot.url}
-          alt={screenshot.name}
-          onClick={() => console.log("Abrir imagen")}
-        />
-      ))}
-    </Slider>
+    <>
+      <Slider {...settings}>
+        {map(screenshots, (screenshot) => (
+          <Image
+            key={screenshot.id}
+            src={screenshot.url}
+            alt={screenshot.name}
+            onClick={() => openImage(screenshot.url)}
+          />
+        ))}
+      </Slider>
+      <Modal open={showModal} onClose={() => setShowModal(false)} size="large">
+        <Image src={urlImage} alt={title} />
+      </Modal>
+    </>
   );
 }
