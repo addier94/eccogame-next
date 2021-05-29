40df33a1b298d534b7e516239528a1a2f0717ffd
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-29 17:50:07 -0400

    get result of search

--- api/game.js
+++ api/game.js
@@ -52,3 +52,15 @@ export async function getGameByUrlApi(path) {
     return null;
   }
 }
+
+export async function searchGamesApi(title) {
+  try {
+    const url = `${BASE_PATH}/games?_q=${title}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
--- pages/search.js
+++ pages/search.js
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from "react";
 import BasicLayout from "../layouts/BassicLayout";
+import { useRouter } from "next/router";
+import { searchGamesApi } from "../api/game";
+import { size } from "lodash";
 
 export default function search() {
+  const [games, setGames] = useState(null);
+  const { query } = useRouter();
+
   useEffect(() => {
     document.getElementById("search-game").focus();
   }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (size(query.query) > 0) {
+        const response = await searchGamesApi(query.query);
+        if (size(response) > 0) setGames(response);
+        else setGames([]);
+      } else {
+        setGames([]);
+      }
+    })();
+  }, [query]);
 
   return (
     <BasicLayout className="search">
