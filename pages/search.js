7deb6940bca706dca59aa61118a8f5addcf82475
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-29 18:03:53 -0400

    rendering data in searching it

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
-import BasicLayout from "../layouts/BassicLayout";
+import { Loader } from "semantic-ui-react";
 import { useRouter } from "next/router";
-import { searchGamesApi } from "../api/game";
 import { size } from "lodash";
+import BasicLayout from "../layouts/BassicLayout";
+import { searchGamesApi } from "../api/game";
+import ListGames from "../components/ListGames";
 
 export default function search() {
   const [games, setGames] = useState(null);
@@ -26,9 +28,13 @@ export default function search() {
 
   return (
     <BasicLayout className="search">
-      <div>
-        <h1>Busqueda...</h1>
-      </div>
+      {!games && <Loader active>Buscando juegos</Loader>}
+      {games && size(games) === 0 && (
+        <div>
+          <h3>No se han encontrado juegos</h3>
+        </div>
+      )}
+      {size(games) > 0 && <ListGames games={games} />}
     </BasicLayout>
   );
 }
