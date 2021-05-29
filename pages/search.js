d27a5e410704cd7c503d1cf60a7976f753e5101b
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-29 16:13:32 -0400

    creating search page

---
+++ pages/search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BasicLayout from "../layouts/BassicLayout";
+
+export default function search() {
+  return (
+    <BasicLayout className="search">
+      <div>
+        <h1>Busqueda...</h1>
+      </div>
+    </BasicLayout>
+  );
+}
