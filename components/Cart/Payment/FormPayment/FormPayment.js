1cd53e7bccabda2907c5288cb7da6d08353acc5c
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-31 13:21:33 -0400

    creating collection agreement with stripe

--- components/Cart/Payment/FormPayment/FormPayment.js
+++ components/Cart/Payment/FormPayment/FormPayment.js
@@ -9,17 +9,34 @@ import useCart from "../../../../hooks/useCart";
 
 export default function FormPayment(props) {
   const { products, address } = props;
+  const [loading, setLoading] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log("Realizando pago...");
+    setLoading(true);
+
+    if (!stripe || !elements) return;
+
+    const cardElement = elements.getElement(CardElement);
+    const result = await stripe.createToken(cardElement);
+
+    if (result.error) {
+      toast.error(result.error.message);
+    } else {
+      console.log(result);
+    }
+
+    setLoading(false);
   };
 
   return (
     <form className="form-payment" onSubmit={handleSubmit}>
-      <div>shi</div>
       <CardElement />
-      <Button type="submit">Pagar</Button>
+      <Button type="submit" loading={loading} disabled={!stripe}>
+        Pagar
+      </Button>
     </form>
   );
 }
