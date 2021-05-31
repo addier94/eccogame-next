8611dcbbf13cf183da152bbea5efa873caef539c
Alfredo Fernandez <alfredo@hotmail.com>
2021-05-30 22:07:49 -0400

    show all list addresses in cart page

--- components/Cart/AddressShipping/AddressShipping.js
+++ components/Cart/AddressShipping/AddressShipping.js
@@ -18,8 +18,43 @@ export default function AddressShipping() {
   }, []);
 
   return (
-    <div>
-      <h1>Addresshipping..</h1>
+    <div className="address-shipping">
+      <div className="title">Dirección de envió</div>
+      <div className="data">
+        {size(addresses) === 0 ? (
+          <h3>
+            No hay ninguna direccion creada{" "}
+            <Link href="/account">
+              <a>Añadir tu primera dirección</a>
+            </Link>
+          </h3>
+        ) : (
+          <Grid>
+            {map(addresses, (address) => (
+              <Grid.Column key={address.id} mobile={16} tablet={8} computer={4}>
+                <Address address={address} />
+              </Grid.Column>
+            ))}
+          </Grid>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Address(props) {
+  const { address } = props;
+  console.log("object ", address);
+
+  return (
+    <div className="address">
+      <p>{address.title}</p>
+      <p>{address.name}</p>
+      <p>{address.address}</p>
+      <p>
+        {address.city}, {address.state} {address.postalCode}
+      </p>
+      <p>{address.phone}</p>
     </div>
   );
 }
