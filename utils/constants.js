1f6511391303c0f92b416f2e3ae8fef7b2f560e9
Alfredo Fernandez <alfredo@hotmail.com>
2021-06-01 10:17:52 -0400

    change favicon

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_PATH = "http://localhost:1337";
+// export const BASE_PATH = "http://localhost:1337";
+export const BASE_PATH = "https://tiendalist.herokuapp.com";
 export const TOKEN = "token";
 export const CART = "cart";
 export const STRIPE_TOKEN = "pk_test_n4hsnT51Js2LUC8WaTfShzfZ00N1R2cbdr";
